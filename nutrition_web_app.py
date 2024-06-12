3c090654afa4710ffbc85e3ef0b5d67638355aed
Ru1chi <116619582+Ru1chi@users.noreply.github.com>
2024-06-12 11:55:20 +0530

    Update nutrition_web_app.py

--- nutrition_web_app.py
+++ nutrition_web_app.py
@@ -17,13 +17,13 @@ import pickle
 
 # Function to load recipe data from an Excel file
 def load_recipe_data():
-    return pd.read_excel(r'C:\Users\sr322\OneDrive\Desktop\ML_Model_Deployment\Nutrition in budget\recipeeees.xlsx')  # Adjust file path as necessary
+    return pd.read_excel(r'recipeeees.xlsx')  # Adjust file path as necessary
 
 # Function to load model and scaler
 def load_model():
-    with open(r'C:\Users\sr322\OneDrive\Desktop\ML_Model_Deployment\Nutrition in budget\kmeans_model11.sav', 'rb') as model_file:
+    with open(r'kmeans_model11.sav', 'rb') as model_file:
         model = pickle.load(model_file)
-    with open(r'C:\Users\sr322\OneDrive\Desktop\ML_Model_Deployment\Nutrition in budget\scaler_model11.sav', 'rb') as scaler_file:
+    with open(r'scaler_model11.sav', 'rb') as scaler_file:
         scaler = pickle.load(scaler_file)
     return model, scaler
 
